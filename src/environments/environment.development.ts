0c3156037ab60dfdf2d985413ed606e9f5b09084
Swapnil <swapnilbopche2016@gmail.com>
2026-03-18 16:36:22 +0530

    set render url in all env files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:1600'
+    apiUrl: 'https://curvix-backend.onrender.com'
 };
 
 //localhost
+// http://localhost:1600
